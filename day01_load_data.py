4e7cada923c4b76366ade738be9a9998c51b1785
Abhishek Prajapati <abhipraj1301@gmail.com>
2026-02-18 21:44:00 +0530

    Delete day01_load_data.py

--- day01_load_data.py
+++
@@ -1,5 +0,0 @@
-# Day 01: Loading Titanic Dataset
-import pandas as pd
-df = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
-print(df.head())
-print(df.info())
